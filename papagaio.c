5c94125f662a1c0af799d519c1c57a461bc3ba8c
marceloassuncao1020 <marceloassuncao1020@gmail.com>
2026-04-15 20:47:27 -0300

    Add files via upload

---
+++ papagaio.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(int argc, char*argv[]){
+    if(argc == 1){
+        printf("Você não digitou nada\n");
+    exit(0);
+
+
+    }
+
+    printf("Você digitou %s\n", argv[1]);
+
+    printf("Marcelo Rigotti")
+}
